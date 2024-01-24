c0dd90d329c566857e8055a0a8b8ba71a0a10bd4
Vojjala Banudeep <147126262+vojjalabanu@users.noreply.github.com>
2024-01-24 19:35:57 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,27 @@
+
+    console.log("hello");
+    let num1=10;
+    let num2=20;
+    let gradePoints=60;
+    let val;
+    let char=5;
+    let charr="5";
+    console.log(num1+num2);
+    if(gradePoints>=90){
+        console.log("A grade");
+    }
+else if(gradePoints<=50){
+    console.log("B grade");
+}
+else{
+console.log("fail");
+}
+for(val=1;val<5;val++){
+    console.log("l love programming");
+}
+    console.log(num1<2 && num2==10);
+   // console.log(char++);
+   // console.log(++char);
+   console.log(char == charr);
+   console.log(char === charr);
+   console.log(!(num1=10));
